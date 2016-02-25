a7c39f08e25157ae1e8880cb7ce8717f1fa4b816
Syafiq Faiz <syafiqfaiz@gmail.com>
2016-02-25 11:29:55 +0800

    can add todo list

--- app/assets/javascripts/components/todos.js.jsx
+++ app/assets/javascripts/components/todos.js.jsx
@@ -22,10 +22,25 @@ var TodoLists = React.createClass({
     });
   },
 
+  handleListSubmit: function(list){
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: list,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div>
         <Todos data={this.state.data} />
+        <TodoForm onListSubmit={this.handleListSubmit}/>
       </div>
     );
   }
@@ -49,9 +64,44 @@ var Todos = React.createClass({
       );
     });
     return (
-      <div className="commentList">
+      <div className="todoList">
         {todoNodes}
       </div>
     );
   }
 });
+
+var TodoForm = React.createClass({
+  getInitialState: function() {
+    return {
+      list: '',
+      done: false  
+    };
+  },
+  handleListChange: function(e){
+    this.setState({list: e.target.value})
+  },
+  handleSubmit: function(e){
+    e.preventDefault();
+    var list = this.state.list.trim();
+    var done = false;
+    if (!list){
+      return;
+    }
+    this.props.onListSubmit({list: list, done: done});
+    this.setState({list: '', done: false})
+  },
+  render: function(){
+    return(
+      <form className="todoForm" onSubmit={this.handleSubmit}>
+        <input 
+          type="text"
+          placeholder="What do you want to do?"
+          value={this.state.list}
+          onChange={this.handleListChange}
+        />
+        <input type="submit" value="Add" />
+      </form>
+    )
+  }
+})
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,10 @@ class TodosController < ApplicationController
     @todos = Todo.all
     render json: @todos
   end
+
+  def create
+    Todo.create(list: params['list'], done: params['done'])
+    @todos = Todo.all
+    render json: @todos
+  end
 end
